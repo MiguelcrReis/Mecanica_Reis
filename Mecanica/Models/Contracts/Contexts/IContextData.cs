bfb35a52b6fbd81f840d277cad7ca42689e7070e
MiguelcrReis <miguelcr.reis@gmail.com>
2022-10-25 21:52:16 -0300

    Implemented IcontextData Implemented IcontextData for People

--- Mecanica/Models/Contracts/Contexts/IContextData.cs
+++ Mecanica/Models/Contracts/Contexts/IContextData.cs
@@ -17,11 +17,35 @@ namespace Mecanica.Models.Contracts.Contexts
         #endregion
 
         #region Cliente
-        void CadastrarCliente(Cliente veiculo);
+        void CadastrarCliente(Cliente cliente);
         List<Cliente> ListarCliente();
         Cliente PesquisarClientePorId(string id);
-        void AtualizarCliente(Cliente veiculo);
+        void AtualizarCliente(Cliente cliente);
         void ExcluirCliente(string id);
+        #endregion
+
+        #region Pessoa
+        void CadastrarPessoa(Pessoa pessoa);
+        List<Pessoa> ListarPessoa();
+        Pessoa PesquisarPessoaPorId(int id);
+        void AtualizarPessoa(Pessoa pessoa);
+        void ExcluirPessoa(int id);
+        #endregion
+
+        #region Pessoa Juridica
+        void CadastrarPessoaJuridica(PessoaJuridica pessoaJuridica);
+        List<PessoaJuridica> ListarPessoaJuridica();
+        PessoaJuridica PesquisarPessoaJuridicaPorId(int id);
+        void AtualizarPessoaJuridica(PessoaJuridica pessoaJuridica);
+        void ExcluirPessoaJuridica(int id);
+        #endregion
+
+        #region Pessoa Fisica
+        void CadastrarPessoaFisica(PessoaFisica pessoaFisica);
+        List<PessoaFisica> ListarPessoaFisica();
+        PessoaFisica PesquisarPessoaFisicaPorId(int id);
+        void AtualizarPessoaFisica(PessoaFisica pessoaFisica);
+        void ExcluirPessoaFisica(int id);
         #endregion
     }
 }
